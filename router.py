181f75d07e795a4ec3ad402be07ad2ea9350491d
bukun <bukun@osgeo.cn>
2018-04-08 16:36:42 +0800

    修改

--- router.py
+++ router.py
@@ -4,14 +4,7 @@
 Router for extor.
 '''
 from torcms.handlers.post_handler import PostHandler
-from extor.handler.user2_handler import User2Handler,UserPartialHandler
-
-
 
 urls = [
     ('/special/(.*)', PostHandler, dict(kind='s', filter_view=True)),
-    ("/user/p/(.*)", UserPartialHandler, dict()),  # Deprecated
-    ("/user_p/(.*)", UserPartialHandler, dict()),  # Deprecated
-    ("/user_j/(.*)", UserPartialHandler, dict()),
-    ("/user/(.*)", User2Handler, dict()),
 ] # type: List[int]
