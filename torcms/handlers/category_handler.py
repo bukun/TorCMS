6928cc5295e6040783ae885679c6f3f4b2550aaf
lihy <lihy@g4>
2019-04-10 15:25:28 +0800

    xiugai

--- torcms/handlers/category_handler.py
+++ torcms/handlers/category_handler.py
@@ -42,7 +42,8 @@ class CategoryAjaxHandler(BaseHandler):
         kwd = {
             'pager': '',
             'title': '最近文档',
-            'kind': kind
+            'kind': kind,
+            'router': config.router_post[kind]
         }
         self.render('admin/{0}/category_list.html'.format(self.tmpl_router),
                     kwd=kwd,
