53eaae317634591d6dec5425e224539ffd2dd8b6
lihy <lihy@g4>
2019-07-15 16:27:58 +0800

    xiugai

--- tester/test_model/test_app_rel.py
+++ tester/test_model/test_app_rel.py
@@ -1,15 +1,34 @@
 # -*- coding:utf-8 -*-
-# from torcms.model.info_relation_model import MRelation,MRelation,MRelation
+
 from torcms.model.relation_model import MRelation
+from torcms.core import tools
 
 
-def Test():
-    assert MRelation()
-    assert MRelation()
-    assert MRelation()
+class TestMRelation():
+    def setup(self):
+        print('setup 方法执行于本类中每条用例之前')
+        self.uid = tools.get_uu4d()
 
+    def test_add_relation(self):
+        app_f = '11111'
+        app_t = '12345'
+        MRelation.add_relation(app_f, app_t)
+        assert True
 
-def Test2():
-    assert MRelation()
-    assert MRelation()
-    assert MRelation()
+    def test_update_relation(self):
+        app_f = '11111'
+        app_t = '12345'
+        MRelation.update_relation(app_f, app_t)
+        assert True
+
+    def test_get_app_relations(self):
+        app_id = '11111'
+
+        MRelation.get_app_relations(app_id)
+        assert True
+
+    def test_delete(self):
+        app_id = '11111'
+
+        MRelation.delete(app_id)
+        assert True
--- tester/test_model/test_collect.py
+++ tester/test_model/test_collect.py
@@ -1,8 +1,15 @@
 # -*- coding:utf-8 -*-
 from torcms.model.collect_model import MCollect
-
-def Test():
-    assert MCollect()
+from torcms.core import tools
 
 
+class TestMCollect():
+    def setup(self):
+        print('setup 方法执行于本类中每条用例之前')
+        self.uid = tools.get_uu4d()
 
+    def test_query_recent(self):
+        user_id = '11111'
+        MCollect.query_recent(user_id)
+        assert True
+    
