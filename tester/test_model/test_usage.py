d5c755ce16f268b51da0aa3ca24075336155cfa0
lihy <lihy@g4>
2019-07-23 11:10:37 +0800

    修改

--- tester/test_model/test_usage.py
+++ tester/test_model/test_usage.py
@@ -1,7 +1,52 @@
 # -*- coding:utf-8 -*-
 from torcms.model.usage_model import MUsage
+from torcms.core import tools
 
-def Test():
-    assert MUsage()
- 
 
+class TestMUsage():
+    def setup(self):
+        print('setup 方法执行于本类中每条用例之前')
+
+        self.postid = '12345'
+        self.userid = tools.get_uuid()
+        self.uid = tools.get_uuid()
+
+    def test_query_by_post(self):
+        MUsage.query_by_post(self.postid)
+        assert True
+
+    def test_get_all(self):
+        MUsage.get_all()
+        assert True
+
+    def test_query_random(self):
+        MUsage.query_random()
+        assert True
+
+    def test_query_recent(self):
+        MUsage.query_recent(self.userid, '1')
+        assert True
+
+    def test_query_recent_by_cat(self):
+        MUsage.query_recent_by_cat(self.userid, '0100', 8)
+        assert True
+
+    def test_query_most(self):
+        MUsage.query_most(self.userid, '0100', 8)
+        assert True
+
+    def test_query_by_signature(self):
+        MUsage.query_by_signature(self.userid, self.postid)
+        assert True
+
+    def test_count_increate(self):
+        MUsage.count_increate(self.uid, '0100', 8)
+        assert True
+
+    def test_add_or_update(self):
+        MUsage.add_or_update(self.userid, self.postid, '1')
+        assert True
+
+    def test_update_field(self):
+        MUsage.update_field(self.uid, self.postid)
+        assert True
--- tester/test_model/test_wiki_his.py
+++ tester/test_model/test_wiki_his.py
@@ -1,7 +1,45 @@
 # -*- coding:utf-8 -*-
 from torcms.model.wiki_hist_model import MWikiHist
+from torcms.core import tools
+
 
 def Test():
     assert MWikiHist()
- 
 
+
+class TestMWikiHist():
+    def setup(self):
+        print('setup 方法执行于本类中每条用例之前')
+
+        self.postid = '12345'
+
+        self.uid = tools.get_uuid()
+
+    def test_get_last(self):
+        MWikiHist.get_last(self.postid)
+        assert True
+
+    def test_get_by_uid(self):
+        MWikiHist.get_by_uid(self.uid)
+        assert True
+
+    def test_update_cnt(self):
+        post_data = {
+            'user_name': 'giser',
+            'cnt_md': 'asdf'
+        }
+        MWikiHist.update_cnt(self.uid, post_data)
+        assert True
+
+    def test_query_by_wikiid(self):
+        MWikiHist.query_by_wikiid(self.uid)
+        assert True
+
+    # def test_create_wiki_history(self):
+    #     post_data = MWikiHist.get_by_uid(self.uid)
+    #     MWikiHist.create_wiki_history(post_data)
+    #     assert True
+
+    def test_delete(self):
+        MWikiHist.delete(self.uid)
+        assert True
