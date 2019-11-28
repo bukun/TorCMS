f1affe673527e0ac82479d294e89fcc7ec67db4c
lihy <lihy@g4>
2019-11-28 16:17:45 +0800

    加测试

--- tester/test_model/test_evaluation.py
+++ tester/test_model/test_evaluation.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
+from torcms.model.category_model import MCategory
 from torcms.model.evaluation_model import MEvaluation
+from torcms.model.post2catalog_model import MPost2Catalog
+from torcms.model.post_model import MPost
 
 
 class TestMEvaluation():
@@ -7,16 +10,66 @@ class TestMEvaluation():
         print('setup 方法执行于本类中每条用例之前')
         self.user_id = '11111'
         self.app_id = 'a1234'
+        self.post_id=self.app_id
+
+
+    def add_message(self, **kwargs):
+
+        p_d = {
+            'title': kwargs.get('title', 'iiiii'),
+            'cnt_md': kwargs.get('cnt_md', 'grgr'),
+            'time_create': kwargs.get('time_create', '1992'),
+            'time_update': kwargs.get('time_update', '1996070600'),
+            'user_name': kwargs.get('user_name', 'yuanyuan'),
+            'view_count': kwargs.get('view_count', 1),
+            'logo': kwargs.get('logo', 'prprprprpr'),
+            'memo': kwargs.get('memo', ''),
+            'order': kwargs.get('order', '1'),
+            'keywords': kwargs.get('keywords', ''),
+            'extinfo': kwargs.get('extinfo', {}),
+            'kind': kwargs.get('kind2', '1'),
+            'valid': kwargs.get('valid', 1),
+
+        }
+        post_id = kwargs.get('post_id', self.post_id)
+
+
+        MPost.create_post(post_id, p_d)
+        user_id=kwargs.get('user_id', self.user_id)
+        MEvaluation.add_or_update(user_id, self.app_id, 1)
+
+
 
     def test_app_evaluation_count(self):
-        MEvaluation.app_evaluation_count(self.app_id)
-        assert True
+        b = MEvaluation.app_evaluation_count(self.app_id)
+
+        p={
+            'user_id':'33336'
+        }
+        self.add_message(**p)
+        p = {
+            'user_id': '54436'
+        }
+        self.add_message(**p)
+
+        a=MEvaluation.app_evaluation_count(self.app_id)
+        assert a==b+2
 
     def test_get_by_signature(self):
-        MEvaluation.get_by_signature(self.user_id, self.app_id)
-        assert True
+        a=MEvaluation.get_by_signature(self.user_id, self.app_id)
+        assert a==None
+        self.add_message()
+        a = MEvaluation.get_by_signature(self.user_id, self.app_id)
+        assert a
 
     def test_add_or_update(self):
         value = 1
         MEvaluation.add_or_update(self.user_id, self.app_id, value)
-        assert True
+        a=MEvaluation.get_by_signature(self.user_id, self.app_id)
+        assert a
+
+    def tearDown(self):
+        print("function teardown")
+        tt = MPost.get_by_uid(self.post_id)
+        if tt:
+            MPost.delete(self.post_id)
