c03b1e0ea7f1829301f97f66d30b2bbc4bf57053
yan1013 <790974751@qq.com>
2020-02-26 11:14:01 +0800

    xiugai`

--- torcms/handlers/leaf_handler.py
+++ torcms/handlers/leaf_handler.py
@@ -33,29 +33,34 @@ class LeafHandler(PostHandler):
         self.kind = kwargs['kind'] if 'kind' in kwargs else '6'
         self.filter_view = kwargs['filter_view'] if 'filter_view' in kwargs else False
 
-    def get(self, *args, **kwargs):
-
-        url_str = args[0]
-        url_arr = self.parse_url(url_str)
-
-        if len(url_arr) == 1 and len(url_str) in [4, 5]:
-            self._view_or_add(url_str)
-        else:
-            kwd = {
-                'title': '',
-                'info': '404. Page not found!',
-            }
-            self.set_status(404)
-            self.render('misc/html/404.html', kwd=kwd,
-                        userinfo=self.userinfo, )
-
     def post(self, *args, **kwargs):
 
         url_str = args[0]
-        print(url_str)
+        logger.info('Post url: {0}'.format(url_str))
         url_arr = self.parse_url(url_str)
-        if url_arr[0] == 'update_order':
+
+        if url_arr[0] in ['_edit']:
+            self.update(url_arr[1])
+        elif url_arr[0] in ['_add']:
+            if len(url_arr) == 2:
+                self.add(uid=url_arr[1])
+            else:
+                self.add()
+        elif url_arr[0] == '_edit_kind':
+            self._change_kind(url_arr[1])
+        elif url_arr[0] in ['_cat_add']:
+            self.add(catid=url_arr[1])
+        elif url_arr[0] == 'update_order':
             self.update_order(url_arr[1], url_arr[2])
+        elif len(url_arr) == 1:
+            # Todo: should not exists.
+            if len(url_str) in [4, 5]:
+                self.add(uid=url_str)
+        elif url_arr[0] == 'rel' and len(url_arr) == 3:
+            self._add_relation(url_arr[1], url_arr[2])
+        else:
+            self.show404()
+
 
     @tornado.web.authenticated
     @privilege.auth_edit
@@ -65,53 +70,3 @@ class LeafHandler(PostHandler):
         '''
         MPost.update_order(uid, order)
 
-    @privilege.auth_view
-    def viewinfo(self, postinfo):
-        '''
-        In infor.
-        '''
-        self.redirect_kind(postinfo)
-
-        if DB_CFG['kind'] == 's':
-            cat_enum1 = []
-
-        else:
-            ext_catid = postinfo.extinfo['def_cat_uid'] if 'def_cat_uid' in postinfo.extinfo else ''
-            ext_catid2 = postinfo.extinfo[
-                'def_cat_uid'] if 'def_cat_uid' in postinfo.extinfo else None
-            cat_enum1 = MCategory.get_qian2(ext_catid2[:2]) if ext_catid else []
-
-        catinfo = None
-        p_catinfo = None
-
-        post2catinfo = MPost2Catalog.get_first_category(postinfo.uid)
-
-        catalog_infors = None
-        if post2catinfo:
-            catinfo = MCategory.get_by_uid(post2catinfo.tag_id)
-            if catinfo:
-                p_catinfo = MCategory.get_by_uid(catinfo.pid)
-                catalog_infors = MPost2Catalog.query_pager_by_slug(catinfo.slug,
-                                                                   current_page_num=1,
-                                                                   order=True)
-
-        kwd = self._the_view_kwd(postinfo)
-
-        MPost.update_misc(postinfo.uid, count=True)
-        if self.get_current_user():
-            MUsage.add_or_update(self.userinfo.uid, postinfo.uid, postinfo.kind)
-
-        tmpl = 'post_{0}/leaf_view.html'.format(self.kind)
-
-        logger.info('The Info Template: {0}'.format(tmpl))
-
-        self.render(tmpl,
-                    kwd=dict(kwd, **self.ext_view_kwd(postinfo)),
-                    postinfo=postinfo,
-                    userinfo=self.userinfo,
-                    catinfo=catinfo,
-                    pcatinfo=p_catinfo,
-                    ad_switch=random.randint(1, 18),
-                    tag_info=MPost2Label.get_by_uid(postinfo.uid),
-                    catalog_infos=catalog_infors,
-                    cat_enum=cat_enum1)
