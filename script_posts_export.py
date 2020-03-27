5586dbc575cac0c8031b336a70c78576b137a486
bukun <bukun@osgeo.cn>
2020-03-27 21:14:46 +0800

    adding sript for post export and import.

---
+++ script_posts_export.py
@@ -0,0 +1,182 @@
+# -*- coding:utf-8 -*-
+'''
+将 OSGeo 数据库中的 Posts 导出。
+'''
+
+import os
+import sys
+import markdown
+
+from torcms.model.post_model import MPost
+from torcms.model.category_model import MCategory
+from torcms.model.post2catalog_model import MPost2Catalog
+import yaml
+from pprint import pprint
+import re
+import shutil
+
+
+# import ruamel.yaml
+
+
+out_ws = './static/xx_mds'
+
+
+def get_img(text):
+    pattern = re.compile('!\[.*?\]\(.*?\)')
+    # pattern = re.compile('''(?|(?<txt>(?<url>(?:ht|f)tps?://\S+(?<=\P{P})))|\(([^)]+)\)\[(\g<url>)\])''')
+    tt = re.findall(pattern, text)
+    for t in tt:
+        print('>' * 40)
+        print(t, t.strip(')').split('(')[-1])
+        print('<' * 40)
+    return tt
+
+
+def do_for_cat(rec):
+    pid = rec.pid
+    cat_id = rec.uid
+    out_base_dir = os.path.join(out_ws, pid, 'ch{}_{}_{}'.format(rec.order, rec.uid,  '_'.join(rec.name.split())))
+    if os.path.exists(out_base_dir):
+        pass
+    else:
+        os.makedirs(out_base_dir)
+
+    post2tag_recs = MPost2Catalog.query_by_catid(cat_id)
+    for post2tag_rec in post2tag_recs:
+        print(post2tag_rec.post_id)
+        postinfo = MPost.get_by_uid(post2tag_rec.post_id)
+        markdown_cnt = postinfo.cnt_md
+        md = markdown.Markdown(extensions=['meta'])
+        html = md.convert(markdown_cnt)
+
+        print(md.Meta)
+        stitle = md.Meta.get('s-title')
+        if stitle:
+            stitle = stitle[0].strip()
+        else:
+            stitle = postinfo.title.strip().replace('/', '-').replace(' ', '_')
+
+        print(stitle)
+
+        out_cat_dir = os.path.join(out_base_dir, 'sec{}_'.format(post2tag_rec.order) + '_'.join(stitle.split()) + '_uid' + post2tag_rec.post_id)
+
+        if os.path.exists(out_cat_dir):
+            pass
+        else:
+            os.makedirs(out_cat_dir)
+
+
+        # 拷贝图片到同一文件夹
+        imgs = get_img(postinfo.cnt_md)
+        for img in imgs:
+            img_url = img.strip(')').split('(')[-1]
+            img_name = os.path.split(img_url)[-1]
+            if img_url.startswith('http'):
+                # 下面代码，可同时处理 http 与 https
+                img_path = './' + img_url[len('http://www.osgeo.cn/'):].strip('/')
+                print(img_path)
+                out_img_path = os.path.join(out_cat_dir, img_name)
+                if os.path.exists(img_path):
+                    shutil.copy(img_path, out_img_path)
+
+                markdown_cnt = markdown_cnt.replace(img_url, img_name)
+
+            elif img_url.startswith('//www.osgeo.cn'):
+                img_path = './' + img_url[len('//www.osgeo.cn/'):].strip('/')
+                print(img_path)
+                out_img_path = os.path.join(out_cat_dir, img_name)
+                if os.path.exists(img_path):
+                    shutil.copy(img_path, out_img_path)
+
+                markdown_cnt = markdown_cnt.replace(img_url, img_name)
+
+            elif img_url.startswith('/static'):
+                img_path = '.' + img_url
+                out_img_path = os.path.join(out_cat_dir, img_name)
+                if os.path.exists(img_path):
+                    shutil.copy(img_path, out_img_path)
+                markdown_cnt = markdown_cnt.replace(img_url, img_name)
+            else:
+                pass
+
+
+        out_file = os.path.join(out_cat_dir , stitle + '.md')
+
+        with open(out_file, 'w') as fout_md:
+            if md.Meta.get('title'):
+                pass
+            else:
+                fout_md.write('title: {}\n'.format(postinfo.title))
+
+            if md.Meta.get('s-title'):
+                pass
+            else:
+                fout_md.write('s-title: {}\n'.format(stitle))
+
+            if md.Meta.get('date'):
+                pass
+            else:
+                fout_md.write('date: {}\n'.format(postinfo.time_update))
+
+            if md.Meta.get('author'):
+                pass
+            else:
+                fout_md.write('author: {}\n\n'.format(postinfo.user_name))
+
+            fout_md.write(markdown_cnt.replace('\r\n', '\n'))
+
+        # return
+
+
+def run_export():
+    all_cats = MCategory.query_all('1')
+    for rec in all_cats:
+        print(rec.uid, rec.pid)
+        if rec.pid == '0000':
+            continue
+
+        else:
+            do_for_cat(rec)
+
+        # return
+
+    # all_recs = MPost.query_all(kind='1', limit=10000)
+    #
+    # for postinfo in all_recs:
+    #     out_arr =            {
+    #             'uid': postinfo.uid,
+    #             'title': postinfo.title,
+    #             'keywords': postinfo.keywords,
+    #             'date': postinfo.date,
+    #             'extinfo': postinfo.extinfo,
+    #             'cnt_md': postinfo.cnt_md,
+    #             'cnt_html': postinfo.cnt_html,
+    #             'kind': postinfo.kind,
+    #             'user_name': postinfo.user_name,
+    #             'logo': '',
+    #         }
+    #
+    #     # print(postinfo.extinfo)
+    #
+    #
+    #     cat_id = postinfo.extinfo.get('def_cat_uid')
+    #
+    #     if cat_id:
+    #         pass
+    #     else:
+    #         print(postinfo.uid)
+    #         print(postinfo.extinfo)
+    #         print('Not found')
+    #         return
+
+    # outdir = os.path.join(out_ws, postinfo.extinfo.get(''))
+
+    # dumper = ruamel.yaml.RoundTripDumper
+
+    # with open('xx_posts.yaml', 'w') as fo:
+    #     yaml.dump(out_arr, fo, Dumper=dumper, allow_unicode=True)
+
+
+if __name__ == '__main__':
+    run_export()
