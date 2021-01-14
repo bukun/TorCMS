6da50060e9150149534254fdc1bb8a9c13f107aa
lihy <lihy@g4>
2021-01-14 15:49:37 +0800

    redis测试

--- tester/test_script/test_redis.py
+++ tester/test_script/test_redis.py
@@ -21,5 +21,9 @@ class TestRedis():
         cfg_var = dir(cfg)
         if 'REDIS_CFG' in cfg_var:
             assert self.torcms_redis['host'] == cfg.REDIS_CFG['host']
+            assert self.torcms_redis['port'] == cfg.REDIS_CFG['port']
+            assert self.torcms_redis['pass'] == cfg.REDIS_CFG['pass']
         else:
             assert self.torcms_redis['host'] == 'localhost'
+            assert self.torcms_redis['port'] == 6379
+            assert self.torcms_redis['pass'] == None
