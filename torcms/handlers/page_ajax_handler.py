48189fa7b384b76139af81108798677adb1c2ad6
lihy0 <790974751@qq.com>
2022-07-14 15:53:31 +0800

    xiugai

--- torcms/handlers/page_ajax_handler.py
+++ torcms/handlers/page_ajax_handler.py
@@ -11,7 +11,8 @@ import tornado.web
 from config import CMS_CFG
 from torcms.handlers.page_handler import PageHandler
 from torcms.model.wiki_model import MWiki
-
+from torcms.model.wiki_hist_model import MWikiHist
+from torcms.model.user_model import MUser
 
 class PageAjaxHandler(PageHandler):
     '''
@@ -39,6 +40,25 @@ class PageAjaxHandler(PageHandler):
             self.view_or_add(url_str)
         else:
             return '{}'
+
+    def post(self, *args, **kwargs):
+        '''
+        用户操作。
+        '''
+        _ = kwargs
+        url_str = args[0]
+        url_arr = self.parse_url(url_str)
+
+        if url_str == 'j_add':
+            self.json_add()
+        elif url_arr[0] == 'j_edit':
+            self.json_edit(url_arr[1])
+        # elif url_str == 'j_delete':
+        #     self.json_delete()
+        elif url_str == 'j_list':
+            self.json_list()
+        elif url_str == 'j_view':
+            self.json_view()
 
     def view(self, rec):
         '''
@@ -88,3 +108,125 @@ class PageAjaxHandler(PageHandler):
         To add the page.
         '''
         self.render('admin/page_ajax/page_add.html', kwd={})
+
+    def json_add(self):
+        '''
+        Add new page.
+        '''
+
+        post_data = self.get_request_arguments()
+
+        post_data['user_name'] = 'admin'
+
+        slug = post_data['slug'].strip()
+        title = post_data['title'].strip()
+        if len(title) < 2:
+            output = {
+                'info': 'Title cannot be less than 2 characters',
+                'code': '0'
+            }
+
+            return json.dump(output, self)
+
+        if MWiki.get_by_uid(slug):
+            self.set_status(400)
+            output = {
+                'info': 'slug already exists',
+                'code': '-1'
+            }
+
+            return json.dump(output, self)
+        else:
+            MWiki.create_page(slug, post_data)
+            tornado.ioloop.IOLoop.instance().add_callback(self.cele_gen_whoosh)
+            output = {
+                'info': 'Added successfully',
+                'code': '1'
+            }
+
+            return json.dump(output, self)
+
+    def json_view(self):
+        '''
+        When access with the slug, It will add the page if there is no record in database.
+        '''
+
+        post_data = self.get_request_arguments()
+        slug = post_data.get('slug', '')
+        print("*" * 50)
+        print(slug)
+        rec_page = MWiki.get_by_uid(slug)
+
+        if rec_page:
+            if rec_page.kind == self.kind:
+
+                out_json = {
+                    'code': '1',
+                    'info': 'success',
+                    'uid': rec_page.uid,
+                    'time_update': rec_page.time_update,
+                    'title': rec_page.title,
+                    'cnt_html': tornado.escape.xhtml_unescape(rec_page.cnt_html),
+                    'cnt_md': rec_page.cnt_md
+                }
+
+                return json.dump(out_json, self)
+            else:
+                out_json = {
+                    'code': '0',
+                    'info': 'Page not found'
+                }
+                return json.dump(out_json, self)
+        else:
+            out_json = {
+                'code': '-1',
+                'info': 'Page not found'
+            }
+            return json.dump(out_json, self)
+
+    def json_list(self):
+        '''
+        View the list of the pages.
+        '''
+        recs = MWiki.query_recent(kind=2)
+        rec_arr = []
+
+        for rec in recs:
+            rec_arr.append({'title': rec.title, 'slug': rec.uid, 'cnt_md': rec.cnt_md})
+
+        out_json = {
+            'code': '1',
+            'info': 'success',
+            'recs': rec_arr
+        }
+        return json.dump(out_json, self)
+
+    def json_edit(self, slug):
+        '''
+        Update the page.
+        '''
+
+        post_data = self.get_request_arguments()
+
+        post_data['user_name'] = 'admin'
+        self.userinfo = MUser.get_by_name('admin')
+        pageinfo = MWiki.get_by_uid(slug)
+
+        cnt_old = tornado.escape.xhtml_unescape(pageinfo.cnt_md).strip()
+        cnt_new = post_data['cnt_md'].strip()
+        if cnt_old == cnt_new:
+            pass
+        else:
+            MWikiHist.create_wiki_history(MWiki.get_by_uid(slug),
+                                          self.userinfo)
+
+        MWiki.update(slug, post_data)
+
+        # tornado.ioloop.IOLoop.instance().add_callback(self.cele_gen_whoosh)
+
+        out_json = {
+            'code': '1',
+            'info': 'success',
+            'uid': slug
+        }
+        return json.dump(out_json, self)
