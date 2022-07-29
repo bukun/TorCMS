95e65eef3c40d51e517e84acc678df4c34b89a6b
lihy0 <790974751@qq.com>
2022-07-29 15:50:28 +0800

    xiugai

--- torcms/model/user_model.py
+++ torcms/model/user_model.py
@@ -267,7 +267,7 @@ class MUser():
         Update the role of the usr.
         '''
         role = postdata['role']
-        authority = postdata['authority']
+        authority = postdata.get('authority','0')
         entry = TabMember.update(role=role, authority=authority).where(
             TabMember.user_name == u_name)
         try:
