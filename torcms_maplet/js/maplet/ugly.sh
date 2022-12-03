019c06aad7d7bed9d83f70e2d45302bb5c94a27c
lihy <790974751@qq.com>
2022-12-03 17:21:47 +0800

    基本完成合并

--- torcms_maplet/js/maplet/ugly.sh
+++
@@ -1,6 +0,0 @@
-# uglifyjs f.js > f.min.js
-coffee -c *.coffee
-uglifyjs fullscreen.js > fullscreenm.js
-uglifyjs fullscreendraw.js > fullscreendrawm.js
-uglifyjs mapshow.js > mapshowm.js
-uglifyjs overlay.js > overlaym.js
