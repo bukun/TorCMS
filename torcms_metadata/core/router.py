bfdf913ff93a970a935440f17e029caaf3269754
lihy <790974751@qq.com>
2022-12-03 20:14:25 +0800

    删除多余代码，登录去掉加密处理

--- torcms_metadata/core/router.py
+++ torcms_metadata/core/router.py
@@ -2,9 +2,7 @@
 
 
 from torcms_metadata.handlers.meta_handler import MetadataHandler
-# from torcms_metadata.handlers.meta_filter_handler import MetaFilterHandler
 
 _urls = [
-    ("/meta_info/(.*)", MetadataHandler, dict(kind='9')),
-    # ("/meta_info_filter/(.*)", MetaFilterHandler, dict(kind='9')),
+    ("/meta_info/(.*)", MetadataHandler, dict(kind='9'))
 ]
