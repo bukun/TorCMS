d8c71af4087d67d4c2b9461d4ce59d4333529b54
lihy <790974751@qq.com>
2022-12-05 16:38:03 +0800

    删除多余模块

--- torcms_metadata/modules/modef.py
+++ torcms_metadata/modules/modef.py
@@ -9,8 +9,6 @@ from torcms_metadata.modules import meta_modules
 _modules = {
 
     'upload_excel': meta_modules.Upload_excel,
-    'meta_catalog_of': meta_modules.MetaCategoryOf,
-    'meta_recent': meta_modules.Meta_Recent,
     'meta_catalog': meta_modules.MetaCategory
 
 }
