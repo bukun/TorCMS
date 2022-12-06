e768e8c9e16889229cb32d1083acabe294ff4e47
lihy <790974751@qq.com>
2022-12-06 11:03:19 +0800

    xiuigai

--- torcms_dde/modules/modef.py
+++ torcms_dde/modules/modef.py
@@ -4,11 +4,7 @@
 define the Core Modules of TorCMS.
 '''
 
-from torcms_metadata.modules import meta_modules
-
 _modules = {
 
-    'upload_excel': meta_modules.Upload_excel,
-    'meta_catalog': meta_modules.MetaCategory
 
 }
