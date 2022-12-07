abd34022b38c74465918dc32122459239f73f1eb
lihy <790974751@qq.com>
2022-12-07 11:29:13 +0800

    创建table，pycws.py脚本可正确运行

---
+++ torcms_dde/scripts/script_migrate_table.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from torcms_dde.model.ext import Records
+from torcms.script.script_init_tabels import create_table
+
+
+def run_init_tables(*args):
+    '''
+    Run to init tables.
+    '''
+
+    try:
+        create_table(Records)
+    except Exception as err:
+        print('--------')
+        print(repr(err))
+
+    print('Migration finished.')
+
+
+if __name__ == '__main__':
+    run_init_tables()
