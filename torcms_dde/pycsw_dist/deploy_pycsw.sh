2086ec01dc9c604ac7707439ec4b4d4f303f32b4
bukun <bukun@osgeo.cn>
2022-12-08 16:06:39 +0800

    bk commit

--- torcms_dde/pycsw_dist/deploy_pycsw.sh
+++ torcms_dde/pycsw_dist/deploy_pycsw.sh
@@ -7,12 +7,18 @@ sudo apt install -y libxml2-dev libxslt1-dev liblzma-dev python3-lxml \
 sudo apt install -y postgis postgresql-plpython3-14
 
 # Setup a virtual environment:
-python3 -m venv ./vpy_csw && . vpy_csw/bin/activate && pip3 install wheel && pip3 install psycopg2-binary
+python3 -m venv --clear ~/usr/vpy_csw \
+  && . ~/usr/vpy_csw/bin/activate \
+  && pip3 install wheel \
+  && pip3 install psycopg2-binary
 
 # git clone https://github.com/geopython/pycsw.git && cd pycsw
-git clone https://gitee.com/gislite/pycsw.git
 
-cd pycsw && pip install -e . && pip install -r requirements-standalone.txt
+the_pwd=`pwd`
+
+git clone https://gitee.com/gislite/pycsw.git $the_pwd/zz_pycsw_drr
+git clone https://gitee.com/gislite/pycsw.git $the_pwd/zz_pycsw_wdc
+cd $the_pwd/zz_pycsw_wdc && pip install -e . && pip install -r requirements-standalone.txt
 
 
 
