05fdd454f11a7eb4416919a1ad4674f395ec5eee
lihy <790974751@qq.com>
2022-12-08 19:33:55 +0800

    xiugai

--- torcms_dde/core/router.py
+++ torcms_dde/core/router.py
@@ -5,6 +5,6 @@ from torcms_dde.handlers.meta_handler import MetadataHandler
 from torcms_dde.handlers.json_handler import JsonHandler
 
 _urls = [
-    ("/dde/(.*)", MetadataHandler, dict(kind='d',  filter_view=True)),
+    ("/metadata/(.*)", MetadataHandler, dict(kind='d',  filter_view=True)),
     ("/json/(.*)", JsonHandler),
 ]
