fdaea607132b2b0da13814bf5a68efbbf15a8381
bukun <bukun@live.cn>
2022-12-15 21:12:37 +0800

    bk commit

--- torcms_taginfo/code_wangjy/h2_txt_huanghang.py
+++
@@ -1,9 +0,0 @@
-#对无实意的高频词转化成stopwords的格式
-
-f1 = open('无实意高频词.txt','r', encoding='utf-8')
-f2 = open('txt-huanghang.txt','w', encoding='utf-8')
-for line in f1:
-    f2.write(line + '\n')
-
-f2.close()
-f1.close()
