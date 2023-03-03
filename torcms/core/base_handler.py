e08eaa7a8088cbf5a99b32c992cd439609ca9847
lihy <790974751@qq.com>
2023-03-03 09:26:30 +0800

    xiugai

--- torcms/core/base_handler.py
+++ torcms/core/base_handler.py
@@ -176,7 +176,16 @@ class BaseHandler(tornado.web.RequestHandler):
         response = requests.get('https://api64.ipify.org?format=json').json()
         return response['ip']
 
-
+    def get_location(self):
+        ip_address = self.get_ip()
+        response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+        location_data = {
+            "ip": ip_address,
+            "city": response.get("city"),
+            "region": response.get("region"),
+            "country": response.get("country_name")
+        }
+        return location_data
 
     def show404(self, kwd=None):
         '''
