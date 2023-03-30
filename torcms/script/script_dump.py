47422cc85b6b15b9b11ae03953c385bcecc0d4b3
bukun <bukun@live.cn>
2023-03-30 08:16:29 +0800

    fix for database dump.

--- torcms/script/script_dump.py
+++ torcms/script/script_dump.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Dump database of PostgreSQL, with date stamp.
-'''
+"""
 import datetime
 import os
 import subprocess
@@ -11,27 +11,31 @@ try:
 except Exception as err:
     print(repr(err))
     DB_CFG = {
-        'db': '',
-        'pass': '',
+        "db": "",
+        "pass": "",
     }
 
-if os.path.exists('tmp'):
+if os.path.exists("tmp"):
     pass
 else:
-    os.mkdir('tmp')
+    os.mkdir("tmp")
 
 
 def run_dump(_):
-    '''
+    """
     Dump database of PostgreSQL
-    '''
-    print('Dumping ... ')
+    """
+    print("Dumping ... ")
 
     current = datetime.datetime.now()
-    dstr = '{}{:0>2d}{:0>2d}-{:0>2d}{:0>2d}'.format(
+    dstr = "{}{:0>2d}{:0>2d}-{:0>2d}{:0>2d}".format(
         current.year, current.month, current.day, current.hour, current.minute
     )
-    cmd = 'export PGPASSWORD={p} && pg_dump -h localhost -p {k} -F c -U {n} {n} > ./tmp/xx_pg_{n}_{d}.bak'.format(
-        n=DB_CFG['db'], p=DB_CFG['pass'], d=dstr, k=DB_CFG.get('port', 5432)
+    cmd = "export PGPASSWORD={p} && pg_dump -h {h} -p {k} -F c -U {n} {n} > ./tmp/xx_pg_{n}_{d}.bak".format(
+        n=DB_CFG["db"],
+        p=DB_CFG["pass"],
+        h=DB_CFG.get("host", "localhost"),
+        k=DB_CFG.get("port", 5432),
+        d=dstr,
     )
     subprocess.run(cmd, shell=True)
