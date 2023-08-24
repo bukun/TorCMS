b1c6e1e0758b57dfdcbdafcb3ea556f2e93a0c99
bukun <bukun@live.cn>
2023-08-24 21:14:52 +0800

    clean the codes.

--- application.py
+++ application.py
@@ -1,51 +1,26 @@
-# coding:utf-8
-
 """
 The Application.
 """
 
 import os
-from pathlib import Path
 import tornado.web
 import torcms.core.uifunction as uifuncs
 from torcms.modules.modef import core_modules
-import router
+import router as app_router
 import torcms.core.router
 from config import config_modules
 from config import SITE_CFG
 from config import ADDONS
 
-# 注册各个应用的模块
-CUR_MODUES = dict(core_modules, **config_modules)  # type: Dict[str, object]
+# 注册各个应用的模块，路由
+APP_MODUES = dict(core_modules, **config_modules)  # type: Dict[str, object]
+APP_URLS = app_router.urls + torcms.core.router.urls
 
-# for wdir in Path(".").iterdir():
-#     if wdir.is_dir() and wdir.name.startswith("torcms_"):
-
-for wdir in ADDONS:
-    # the_file = Path(wdir) / 'modules/modef.py'
-    # if the_file.exists():
-    #     pass
-    # else:
-    #     continue
-    the_mod = f"{wdir}.modules.modef"
-    _mod = __import__(the_mod)
-    CUR_MODUES = dict(CUR_MODUES, **_mod.modules.modef._modules)
-
-# 注册路由
-
-# for wdir in Path(".").iterdir():
-#     if wdir.is_dir() and wdir.name.startswith("torcms_"):
-#         the_file = wdir / 'core/router.py'
-#         if the_file.exists():
-#             pass
-#         else:
-#             continue
-urls = router.urls + torcms.core.router.urls
-for wdir in ADDONS:
-    the_mod = f"{wdir}.core.router"
-    _mod = __import__(the_mod)
-    urls = urls + _mod.core.router._urls
-
+for addon in ADDONS:
+    _imp_mod = __import__(f"{addon}.modules.modef")
+    _imp_router = __import__(f"{addon}.core.router")
+    APP_MODUES = dict(APP_MODUES, **_imp_mod.modules.modef._modules)
+    APP_URLS = APP_URLS + _imp_router.core.router._urls
 
 SETTINGS = {
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
@@ -53,8 +28,8 @@ SETTINGS = {
     "debug": SITE_CFG["DEBUG"],
     "cookie_secret": SITE_CFG["cookie_secret"],
     "login_url": "/user/login",
-    "ui_modules": CUR_MODUES,
+    "ui_modules": APP_MODUES,
     "ui_methods": uifuncs,
 }
 
-APP = tornado.web.Application(handlers=urls, **SETTINGS)
+APP = tornado.web.Application(handlers=APP_URLS, **SETTINGS)
