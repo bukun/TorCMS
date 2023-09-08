9e51f877b7ec26b566835e39ddd0dc642d21c90f
lihy <790974751@qq.com>
2023-09-08 15:38:35 +0800

    xg

--- torcms_maplet/js/overlay.js
+++ torcms_maplet/js/overlay.js
@@ -1,1 +1,37 @@
-(function(){$(document).ready(function(){var app_arr,app_url,baseMaps,ii,jj,lyrs,map,mycars,osm,overlayMaps;for(lyrs=new L.LayerGroup,mycars=new Array,app_url=$("#app_ctrl").val(),app_arr=app_url.split("/"),jj=0;jj<app_arr.length;)mycars[jj]=L.tileLayer.wms("https://tile.osgeo.cn/service?",{layers:"mp"+app_arr[jj].substring(1),format:"image/png",transparent:!0,attribution:"Maplet"}),mycars[jj].addTo(lyrs),jj++;var osm=L.tileLayer.chinaProvider("TianDiTu.Normal.Annotion",{maxZoom:18,minZoom:1}),osm1=L.tileLayer.chinaProvider("TianDiTu.Normal.Map",{maxZoom:18,minZoom:1}),the_basemap=L.layerGroup([osm1,osm]);for(map=L.map("map",{center:[vlat,vlon],zoom:vzoom_current,maxZoom:vzoom_max,minZoom:vzoom_min,layers:[lyrs]}),the_basemap.addTo(map),baseMaps={osm:the_basemap},overlayMaps={},ii=0;ii<app_arr.length;)overlayMaps[app_arr[ii]]=mycars[ii],ii++;return L.control.layers(baseMaps,overlayMaps).addTo(map)})}).call(this);
+(function () {
+    $(document).ready(function () {
+        var baseMaps, ii, jj, lyrs, map, mycars, osm, overlayMaps, aa, mp_uid;
+        var app_url = $("#app_ctrl").val(), app_arr = app_url.split("/")
+        for (aa = 0; aa < app_arr.length;) {
+            if (app_arr[aa].substring(0, 1) == 'm') {
+
+                mp_uid = "mp" + app_arr[aa].substring(1, 5);
+
+            } else {
+
+                mp_uid = "qn" + app_arr[aa].substring(1, 5);
+
+            }
+            aa++
+        }
+
+        for (lyrs = new L.LayerGroup, mycars = new Array, jj = 0; jj < app_arr.length;)
+            mycars[jj] = L.tileLayer.wms("https://tile.osgeo.cn/service?", {
+                layers: mp_uid,
+                format: "image/png",
+                transparent: !0,
+                attribution: "Maplet"
+            }), mycars[jj].addTo(lyrs), jj++;
+        var osm = L.tileLayer.chinaProvider("TianDiTu.Normal.Annotion", {maxZoom: 18, minZoom: 1}),
+            osm1 = L.tileLayer.chinaProvider("TianDiTu.Normal.Map", {maxZoom: 18, minZoom: 1}),
+            the_basemap = L.layerGroup([osm1, osm]);
+        for (map = L.map("map", {
+            center: [vlat, vlon],
+            zoom: vzoom_current,
+            maxZoom: vzoom_max,
+            minZoom: vzoom_min,
+            layers: [lyrs]
+        }), the_basemap.addTo(map), baseMaps = {osm: the_basemap}, overlayMaps = {}, ii = 0; ii < app_arr.length;) overlayMaps[app_arr[ii]] = mycars[ii], ii++;
+        return L.control.layers(baseMaps, overlayMaps).addTo(map)
+    })
+}).call(this);
