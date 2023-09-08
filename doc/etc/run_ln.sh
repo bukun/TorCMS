210986f8f91f0621afe712b73079085533fa6ffa
bukun <bukun@live.cn>
2023-09-08 10:27:48 +0800

    bk commit

--- doc/etc/run_ln.sh
+++ doc/etc/run_ln.sh
@@ -1,15 +1,21 @@
-if [ -e /etc/nginx/sites-enabled/ng_torcms_amis.nginx ]
-  then sudo rm /etc/nginx/sites-enabled/ng_torcms_amis.nginx
-fi
+sudo apt install -y supervisor
 
-if [ -e /etc/supervisor/conf.d/spv_torcms.conf ]
-  then sudo rm /etc/supervisor/conf.d/spv_torcms.conf
-fi
+#if [ -e /etc/nginx/sites-enabled/ng_torcms_amis.nginx ]
+#  then sudo rm /etc/nginx/sites-enabled/ng_torcms_amis.nginx
+#fi
+#
+#if [ -e /etc/supervisor/conf.d/spv_torcms.conf ]
+#  then sudo rm /etc/supervisor/conf.d/spv_torcms.conf
+#fi
 
-sudo ln -s `pwd`/ng_torcms_amis.nginx /etc/nginx/sites-enabled/ng_torcms_amis.nginx
-sudo ln -s `pwd`/spv_torcms.conf /etc/supervisor/conf.d/spv_torcms.conf
+sudo cp `pwd`/ng_torcms_amis.nginx /etc/nginx/sites-enabled/ng_torcms_amis.nginx
+sudo cp `pwd`/spv_torcms.conf /etc/supervisor/conf.d/spv_torcms.conf
 
 sudo chmod 755 /etc/nginx/sites-enabled/ng_torcms_amis.nginx
 sudo chmod 755 /etc/supervisor/conf.d/spv_torcms.conf
 
 
+sudo supervisorctl reload
+sudo service nginx restart
+
+sudo supervisorctl status
