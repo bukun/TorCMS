31910ba419d8712106270218574a5a405c564011
lihy <790974751@qq.com>
2023-12-01 14:14:39 +0800

    xg

--- torcms/handlers/static_handler.py
+++ torcms/handlers/static_handler.py
@@ -17,16 +17,21 @@ class StaticHandler(BaseHandler):
         super().initialize()
 
     def get(self, *args, **kwargs):
-        url_str = args[0]
-        kwd = {
-            'pager': '',
-        }
-        static_html_file = 'templates/static_pages/{0}'.format(url_str)
-        if os.path.exists(static_html_file) and os.path.isfile(static_html_file):
-            kwd['info'] = ''
-            self.render(
-                'static_pages/{0}'.format(url_str), kwd=kwd, userinfo=self.userinfo
-            )
-        else:
+        kwd = {}
+        if len(args) > 2:
             kwd['info'] = '404! Page not found.'
             self.render('misc/html/404.html', kwd=kwd, userinfo=self.userinfo)
+        else:
+            url_str = args[0]
+            kwd = {
+                'pager': '',
+            }
+            static_html_file = 'templates/static_pages/{0}'.format(url_str)
+            if os.path.exists(static_html_file) and os.path.isfile(static_html_file):
+                kwd['info'] = ''
+                self.render(
+                    'static_pages/{0}'.format(url_str), kwd=kwd, userinfo=self.userinfo
+                )
+            else:
+                kwd['info'] = '404! Page not found.'
+                self.render('misc/html/404.html', kwd=kwd, userinfo=self.userinfo)
