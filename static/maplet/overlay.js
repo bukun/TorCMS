bddf8de0bb04e35a3b4a961cdfcf5bd33ac9d772
lihy <790974751@qq.com>
2024-02-28 10:47:03 +0800

    xg

--- static/maplet/overlay.js
+++ static/maplet/overlay.js
@@ -1,27 +1,31 @@
 (function () {
     $(document).ready(function () {
-        var baseMaps, ii, jj, lyrs, map, mycars, osm, overlayMaps, aa, mp_uid;
-        var app_url = $("#app_ctrl").val(), app_arr = app_url.split("/")
-        for (aa = 0; aa < app_arr.length;) {
-            if (app_arr[aa].substring(0, 1) == 'm') {
+        var app_arr, app_url, baseMaps, ii, jj, lyrs, map, mycars, osm, overlayMaps, mp_uid;
+        for (lyrs = new L.LayerGroup, mycars = new Array, app_url = $("#app_ctrl").val(), app_arr = app_url.split("/"), jj = 0; jj < app_arr.length;)
 
-                mp_uid = "mp" + app_arr[aa].substring(1, 5);
+            if (app_arr[jj].substring(0, 1) == 'm') {
+
+                mp_uid = "mp" + app_arr[jj].substring(1, 5);
+                mycars[jj] = L.tileLayer.wms("https://tile.osgeo.cn/service?", {
+                    layers: mp_uid,
+                    format: "image/png",
+                    transparent: !0,
+                    attribution: "DRRKS"
+                }), mycars[jj].addTo(lyrs), jj++;
 
             } else {
 
-                mp_uid = "qn" + app_arr[aa].substring(1, 5);
+                mp_uid = "qn" + app_arr[jj].substring(1, 5);
+                mycars[jj] = L.tileLayer.wms("https://tile.osgeo.cn/service?", {
+                    layers: mp_uid,
+                    format: "image/png",
+                    transparent: !0,
+                    attribution: "DRRKS"
+                }), mycars[jj].addTo(lyrs), jj++;
+
 
             }
-            aa++
-        }
 
-        for (lyrs = new L.LayerGroup, mycars = new Array, jj = 0; jj < app_arr.length;)
-            mycars[jj] = L.tileLayer.wms("https://tile.osgeo.cn/service?", {
-                layers: mp_uid,
-                format: "image/png",
-                transparent: !0,
-                attribution: "Maplet"
-            }), mycars[jj].addTo(lyrs), jj++;
         var osm = L.tileLayer.chinaProvider("TianDiTu.Normal.Annotion", {maxZoom: 18, minZoom: 1}),
             osm1 = L.tileLayer.chinaProvider("TianDiTu.Normal.Map", {maxZoom: 18, minZoom: 1}),
             the_basemap = L.layerGroup([osm1, osm]);
@@ -33,5 +37,7 @@
             layers: [lyrs]
         }), the_basemap.addTo(map), baseMaps = {osm: the_basemap}, overlayMaps = {}, ii = 0; ii < app_arr.length;) overlayMaps[app_arr[ii]] = mycars[ii], ii++;
         return L.control.layers(baseMaps, overlayMaps).addTo(map)
+
+
     })
 }).call(this);
