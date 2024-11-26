e47a7145619a5ed0061cc860cc213e72c929b5d8
lihy <790974751@qq.com>
2024-11-26 11:34:20 +0800

    xg

--- djadmin/mysite/settings.py
+++ djadmin/mysite/settings.py
@@ -412,9 +412,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hans'
+# LANGUAGE_CODE = 'zh-hans'
+#
+# TIME_ZONE = 'Asia/Shanghai'
 
-TIME_ZONE = 'Asia/Shanghai'
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
 
 USE_I18N = True
 
