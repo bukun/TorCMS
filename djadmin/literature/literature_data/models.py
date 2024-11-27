d1467a257b5f8ea4c220e0643914d4ea8abf5ac4
lihy <790974751@qq.com>
2024-11-27 11:29:39 +0800

    xg

--- djadmin/literature/literature_data/models.py
+++ djadmin/literature/literature_data/models.py
@@ -18,15 +18,16 @@ class Literature(basemodel):
     theme = models.CharField(blank=True, null=False, max_length=255, verbose_name="主题")
     type = models.CharField(blank=True, null=False, max_length=255, verbose_name="类型")
     cnt_md = MDTextField(verbose_name="简介", null=True, blank=True)
-    pub_date = models.ForeignKey(LiteratureDate, on_delete=models.CASCADE, blank=True, null=True,
-                                 related_name='literature_data', verbose_name='发布日期')
-    author = models.ManyToManyField(LiteratureAuthor, blank=True,null=True, related_name='literature_data', verbose_name='作者')
-    label = models.ManyToManyField(LiteratureLabel, related_name='literature_data', verbose_name='标签', blank=True)
     category = models.ForeignKey(LiteratureCatagory, on_delete=models.CASCADE, blank=True, null=True,
                                  related_name='literature_data', verbose_name='分类名称')
     logo = models.ImageField(upload_to='literature/imgs/', max_length=255, null=True, blank=True,
                              verbose_name="图片")
     file = models.FileField(upload_to='literature/files/', null=True, blank=True, verbose_name="文件")
+    pub_date = models.ForeignKey(LiteratureDate, on_delete=models.CASCADE, blank=True, null=True,
+                                 related_name='literature_data', verbose_name='发布日期')
+    author = models.ManyToManyField(LiteratureAuthor, blank=True,null=True, related_name='literature_data', verbose_name='作者')
+    label = models.ManyToManyField(LiteratureLabel, related_name='literature_data', verbose_name='标签', blank=True)
+
     sites = models.ManyToManyField(Site, blank=True, related_name='literature', verbose_name='Site')
 
     def __str__(self):
