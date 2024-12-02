5f1ae1b6a463d74d64c7f891c66ded1e9f8829ef
Ybiao <478747656@qq.com>
2024-12-02 16:21:34 +0800

    xiugai

--- djadmin/public_model/portal_index/views.py
+++ djadmin/public_model/portal_index/views.py
@@ -10,12 +10,9 @@ from base.models import get_template
 from django.contrib.auth import get_user_model
 from django_filters import rest_framework
 from django.views.generic import ListView
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.sites.models import Site
-from base.models import get_template
-
+from data.dataset.models import dataset
+from jupyters.jupyter_data.models import Jupyter
 
 parent_template = get_template()
 current_site = Site.objects.get_current()
@@ -31,4 +28,16 @@ def ydyl_index(request):
 
 
     return render(request, 'portal_index/ydyl_index.html', context)
+
+def wds_index(request):
+
+    post_data = Document.objects.filter(sites__id=current_site.id,category__isnull=False)[:4]
+
+    datalist = dataset.objects.filter(sites__id=current_site.id, category__isnull=False)[:4]
+    expert_data = LiteratureAuthor.objects.filter(sites__id=current_site.id)[:3]
+    jupyter_data = Jupyter.objects.filter(sites__id=current_site.id, category__isnull=False)[:3]
+    context = {'post_data': post_data, 'expert_data': expert_data, 'parent_template': parent_template,'dataset_data': datalist,'jupyter_data': jupyter_data}
+
+
+    return render(request, 'portal_index/wds_index.html', context)
 
