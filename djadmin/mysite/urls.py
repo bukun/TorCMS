ec70302aa64ab81941008a8f0e33d758f04ef883
lihy <790974751@qq.com>
2024-12-09 15:10:51 +0800

    xg

--- djadmin/mysite/urls.py
+++ djadmin/mysite/urls.py
@@ -212,4 +212,4 @@ urlpatterns = (
 )
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += tuple(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
