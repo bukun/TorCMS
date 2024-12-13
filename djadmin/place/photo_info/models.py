c6fa6da80a25eedb12e73dfd9d09dcd8d2177220
Ybiao <478747656@qq.com>
2024-12-13 14:46:25 +0800

    修改内容增加顺序

--- djadmin/place/photo_info/models.py
+++ djadmin/place/photo_info/models.py
@@ -11,12 +11,11 @@ User = get_user_model()
 class Photoinfo(basemodel):
 
     title = models.CharField(blank=True, null=False, max_length=255, verbose_name="标题")
-    cnt_md = MDTextField(verbose_name="内容", null=True, blank=True)
-    lat = models.CharField(blank=True, null=True, default=0, max_length=255, verbose_name="纬度")
-    lon = models.CharField(blank=True, null=True, default=1, max_length=255, verbose_name="经度")
     logo = models.ImageField(upload_to='photo_info/imgs/', max_length=255, null=True, blank=True,
                              verbose_name="图片")
-
+    lat = models.CharField(blank=True, null=True, default=0, max_length=255, verbose_name="纬度")
+    lon = models.CharField(blank=True, null=True, default=1, max_length=255, verbose_name="经度")
+    cnt_md = MDTextField(verbose_name="内容", null=True, blank=True)
 
 
     def __str__(self):
