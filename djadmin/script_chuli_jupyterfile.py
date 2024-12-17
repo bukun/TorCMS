9ebedd02e49f2fd1b7081106be14326e5b8aaffe
team <118171@qq.com>
2024-12-17 17:18:13 +0800

    bk commit.

--- djadmin/script_chuli_jupyterfile.py
+++ djadmin/script_chuli_jupyterfile.py
@@ -5,32 +5,55 @@ import random
 import django
 import nbformat
 from pathlib import Path
-file_path = os.path.abspath(os.path.join(os.path.realpath(__file__), '..'))
+
+# file_path = os.path.abspath(os.path.join(os.path.realpath(__file__), '..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
-if django.VERSION >= (1, 7):  # 自动判断版本
-    django.setup()
-
-from jupyters.jupyter_data.models import Jupyter
 
 django.setup()
 
+from jupyters.jupyter_data.models import Jupyter
+
+
+def chuli_db():
+    for obj in Jupyter.objects.all():
+        print('=' * 40)
+        print(obj.file_id)
+        # file_src = os.path.join(obj.file.path)
+
+        if obj.file_id.startswith('jt') and len(str(obj.file_id)) == 6:
+            # file_id = obj.file_id
+            pass
+        else:
+            # 处理已经上传的jupyter信息，但fileid值有问题的地方。
+            print(obj.file_id)
+            file_id = gen_id()
+            while Jupyter.objects.filter(file_id=file_id):
+                file_id = gen_id()
+            print(file_id)
+            obj.file_id = file_id
+            obj.save()
+
+
+
+def gen_id():
+    return 'jt' + ''.join(random.sample(list('0123456789abcdef'), 4))
+
 
 def chuli_jupyterfile():
+    new_jupyter_path = Path(__file__).parent / 'xx_new_jupyter'
+    if new_jupyter_path.exists():
+        pass
+    else:
+        new_jupyter_path.mkdir()
     for obj in Jupyter.objects.all():
         print(obj.file_id)
         file_src = os.path.join(obj.file.path)
+        print(obj.file.path)
+        print(file_src)
 
-        if obj.file_id.startswith('jt') and len(str(obj.file_id))==6:
-            file_id = obj.file_id
-        else:
-            # 处理已经上传的jupyter信息，但fileid值有问题的地方。
-            file_id = 'jt' + ''.join(random.sample(list('0123456789abcdefghijklmnopqrstuvwxyz'), 4))
-            while Jupyter.objects.filter(file_id=file_id):
-                file_id = 'jt' + ''.join(random.sample(list('0123456789abcdefghijklmnopqrstuvwxyz'), 4))
-            obj.file_id = file_id
-            obj.save()
+        file_id = obj.file_id
 
         with open(file_src, 'r', encoding='utf-8') as f:
             notebook = nbformat.read(f, as_version=4)
@@ -41,15 +64,19 @@ def chuli_jupyterfile():
             if cell.cell_type == 'code':
                 new_nb.cells.append(cell)
 
-        new_jt_file = file_path + '/xx_new_jupyter'
-        file_name =  file_id + '.ipynb'
-        if not os.path.exists(new_jt_file):
-            os.makedirs(new_jt_file)
-        for roo,dirs,files in os.walk(new_jt_file):
-            if file_name in files:
-                pass
-            else:
-                with open(new_jt_file + '/' + file_id + '.ipynb', 'w', encoding='utf-8') as f:
-                    nbformat.write(new_nb, f)
+        file_name = file_id + '.ipynb'
+        out_file = new_jupyter_path / file_name
+        print(out_file)
+
+        if out_file.exists():
+            print('PASS: ', out_file)
+            pass
+        else:
+            with open(out_file, 'w', encoding='utf-8') as f:
+                nbformat.write(new_nb, f)
+            # out_file.write_bytes(new_nb)
+
+
 if __name__ == "__main__":
-    chuli()
+    # chuli_db()
+    chuli_jupyterfile()
---
+++ djadmin/sh_push_jufile.sh
@@ -0,0 +1,1 @@
+rsync -av ./xx_new_jupyter bk@w8:/home/bk/gedock/
