125e9ff3721a0fdb3ddbeb8de385fd81b4967c4e
lihy <790974751@qq.com>
2024-12-19 08:50:16 +0800

    xg

--- djadmin/users/models.py
+++ djadmin/users/models.py
@@ -50,11 +50,15 @@ class myuser(AbstractUser):
     def save(self, *args, **kwargs):
         super(myuser, self).save(*args, **kwargs)
 
-        while True:
-            random_number = random.randint(10000, 65535)
-            try:
-                existing_object = myuser.objects.get(jupyter_port=random_number)
-            except myuser.DoesNotExist:
-                self.jupyter_port = random_number
-                break
+        if not self.pk:
+            while True:
+                random_number = random.randint(10000, 65535)
+                try:
+                    existing_object = myuser.objects.get(jupyter_port=random_number)
+                except myuser.DoesNotExist:
+                    self.jupyter_port = random_number
+                    break
+        else:
+            temp_obj = myuser.objects.get(pk=self.pk)
+            self.jupyter_port = temp_obj.jupyter_port
         super(myuser, self).save(*args, **kwargs)
