ad0b82e2e06f81771b7813dc31adf93a8fbece49
Ybiao <478747656@qq.com>
2024-12-24 16:25:07 +0800

    xiugai

--- djadmin/iga/iga_room/models.py
+++ djadmin/iga/iga_room/models.py
@@ -11,37 +11,33 @@ from django.utils.safestring import mark_safe
 
 
 class iga_room(basemodel):
+    building = models.CharField(blank=True, default='', max_length=255, verbose_name="所属楼")
+    floor = models.ForeignKey(iga_floor, on_delete=models.CASCADE, blank=True, null=True,
+                                 related_name='iga_room', verbose_name='所在楼层')
+    duty =  models.ForeignKey(iga_staff, on_delete=models.CASCADE, blank=True, null=True,
+                                 related_name='iga_room_duty', verbose_name='责任人')
+    num = models.CharField(blank=True, null=False, max_length=255, verbose_name="房间号")
+    group = models.ManyToManyField(iga_group, blank=True,
+                                   related_name='iga_room', verbose_name='学科组名称')
     title = models.CharField(blank=True, null=False, max_length=255,
                              choices=[('1', '公共设施'), ('2', '办公室'),('3', '实验室'),('4', '档案室'),
                                       ('5', '会议室')],
                              default='2',
                              verbose_name="用途")
-    num = models.CharField(blank=True, null=False, max_length=255, verbose_name="房间号")
-    staff = models.CharField(blank=True, null=False, max_length=255, verbose_name="备注")
-    photo = models.ImageField(upload_to='iga_room/imgs/', max_length=255, null=True, blank=True,
-                             verbose_name="照片")
-    area = models.CharField(blank=True,default='', max_length=255, verbose_name="面积")
-    areafloat = models.FloatField(blank=True, null=True, default=0,verbose_name="面积值")
-
     attention = models.CharField(blank=True, null=False, max_length=255,
                              choices=[('1', '安全'), ('2', '危险')],
                              default='',
                              verbose_name="注意事项")
 
-    group = models.ManyToManyField(iga_group, blank=True,
-                                 related_name='iga_room', verbose_name='学科组名称')
-    floor = models.ForeignKey(iga_floor, on_delete=models.CASCADE, blank=True, null=True,
-                                 related_name='iga_room', verbose_name='所在楼层')
+    photo = models.ImageField(upload_to='iga_room/imgs/', max_length=255, null=True, blank=True,
+                             verbose_name="照片")
+    area = models.CharField(blank=True,default='', max_length=255, verbose_name="面积")
+    areafloat = models.FloatField(blank=True, null=True, default=0,verbose_name="面积值")
 
-    duty =  models.ForeignKey(iga_staff, on_delete=models.CASCADE, blank=True, null=True,
-                                 related_name='iga_room_duty', verbose_name='责任人')
-
+    staff = models.CharField(blank=True, null=False, max_length=255, verbose_name="备注")
     operaters = models.ManyToManyField(iga_staff, blank=True,
                                  related_name='iga_room_operaters',verbose_name="使用人员")
 
-    building = models.CharField(blank=True, default='', max_length=255, verbose_name="所属楼")
-
-    cnt_md = MDTextField(verbose_name="内容", default='', blank=True)
 
     sites = models.ManyToManyField(Site, blank=True, related_name='iga_room', verbose_name='Site')
 
