787b9d5f5dc6c8fbea1ab474239e7e269d36c8bd
lihy <790974751@qq.com>
2025-01-15 11:18:38 +0800

    xg

--- djadmin/mysite/urls.py
+++ djadmin/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import json
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
