702f478ef36d0bee4aa2a6293e4c86a6326d2813
BBM <1057246055@qq.com>
2025-03-12 16:12:27 +0800

    add page ,reply

---
+++ test_http/test_reply.py
@@ -0,0 +1,23 @@
+import json
+import unittest
+
+from tornado.testing import AsyncHTTPTestCase
+
+from server import APP
+
+from torcms.model.reply_model import MReply
+
+reply_list = MReply.query_all()
+
+
+class TestTornado(AsyncHTTPTestCase):
+    def get_app(self):
+        return APP
+
+
+    def test_reply_list(self):
+        for reply in reply_list:
+
+            response = self.fetch('/reply/more/{0}'.format(reply))
+            self.assertEqual(response.code, 200)
+
