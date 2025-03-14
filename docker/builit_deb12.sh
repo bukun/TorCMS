61fbb01868245b4a6b31e834c3533c868206a013
bukun <bukun@osgeo.cn>
2025-03-14 11:33:08 +0800

    fix.

--- docker/builit_deb12.sh
+++ docker/builit_deb12.sh
@@ -4,6 +4,6 @@ podman build -t torcms-debian12 -f Dockerfile_deb12
 
 rm -f ../admin_torcms/migrations/00*.py
 
-podman run -it -v  ../../TorCMS:/coding/TorCMS bk-torcms-debian12 bash -c \
+podman run -it -v  ../../TorCMS:/coding/TorCMS torcms-debian12 bash -c \
   "/etc/init.d/postgresql start && sudo -u postgres psql -c \"CREATE ROLE torcms WITH LOGIN PASSWORD '111111';\" && sudo -u postgres psql -c \"CREATE DATABASE torcms WITH OWNER torcms;\" && /vpy/bin/python manage.py makemigrations && /vpy/bin/python manage.py migrate && /vpy/bin/python3 helper.py -i init && /vpy/bin/python3 -m pytest torcms/tests"
 
--- docker/builit_ubu22.sh
+++ docker/builit_ubu22.sh
@@ -4,6 +4,6 @@ podman build -t torcms-ubun22 -f Dockerfile_ubu22
 
 rm -f ../admin_torcms/migrations/00*.py
 
-podman run -it -v  ../../TorCMS:/coding/TorCMS bk-torcms-ubun22 bash -c \
+podman run -it -v  ../../TorCMS:/coding/TorCMS torcms-ubun22 bash -c \
   "/etc/init.d/postgresql start && sudo -u postgres psql -c \"CREATE ROLE torcms WITH LOGIN PASSWORD '111111';\" && sudo -u postgres psql -c \"CREATE DATABASE torcms WITH OWNER torcms;\" && /vpy/bin/python manage.py makemigrations && /vpy/bin/python manage.py migrate && /vpy/bin/python3 helper.py -i init && /vpy/bin/python3 -m pytest torcms/tests"
 
