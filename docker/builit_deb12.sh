856f10ef30185fb7d044a040a6decf7ae28b2e0c
bukun <bukun@osgeo.cn>
2025-03-14 11:14:17 +0800

    podman tester.

---
+++ docker/builit_deb12.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+podman build -t torcms-debian12 -f Dockerfile_deb12
+
+rm ../../admin_torcms/migrations/00*.py
+
+podman run -it -v  ../../../TorCMS:/coding/TorCMS bk-torcms-debian12 bash -c \
+  "/etc/init.d/postgresql start && sudo -u postgres psql -c \"CREATE ROLE torcms WITH LOGIN PASSWORD '111111';\" && sudo -u postgres psql -c \"CREATE DATABASE torcms WITH OWNER torcms;\" && /vpy/bin/python manage.py makemigrations && /vpy/bin/python manage.py migrate && /vpy/bin/python3 helper.py -i init && /vpy/bin/python3 -m pytest torcms/tests"
+
